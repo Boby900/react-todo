f5285fc9e4ade409273d28a5856a02be5f25090c
Boby900 <bobytiwari310@gmail.com>
2024-06-24 15:23:49 +0530

    chore: Add test script for vitest

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,9 @@
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+
+import Home from "../src/app/page";
+ 
+test('Home', () => {
+  render(<Home />)
+  expect(screen.getByRole('heading', { level: 1, name: `Welcoming you on board, is it faster, let's see` })).toBeDefined()
+})
