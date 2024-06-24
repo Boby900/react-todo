ca631db620686c4c45d08bc2f08fc40d10a102a4
Boby900 <bobytiwari310@gmail.com>
2024-06-24 15:11:13 +0530

    chore: Add empty div to improve page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="font-sans text-3xl grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
      
       <h1>Welcoming you on board, is it faster, let&apos;s see</h1>
+      <div></div>
     </div>
   );
 }
