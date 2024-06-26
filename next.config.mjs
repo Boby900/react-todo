86c127829b2de123f57c4ce690d2ba6fee215b34
Boby900 <bobytiwari310@gmail.com>
2024-06-26 11:24:44 +0530

    refactor: Update webpack config to resolve '@' alias to 'src' directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+
 const nextConfig = {
   experimental: {
     reactCompiler: true,
   },
-
-  webpack: (config, { isServer }) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    return config;
-  }
-
 };
-
 export default nextConfig;
