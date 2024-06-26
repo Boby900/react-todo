a8db9d343d41f7ce53cfab34248284bab5390e5f
Boby900 <bobytiwari310@gmail.com>
2024-06-26 09:38:57 +0530

    chore: Update npm dependencies and add new packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 
 import _React from 'react';
 import { SignIn } from '@/components/sign-in';
+import { ProfileForm } from '@/components/Form';
 export default function Home() {
   return (
     <div className="font-sans text-3xl grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
      
-      <h1>Welcoming you on board, is it faster, let&apos;s see</h1>
-      <div>
-        <SignIn />
-      </div>
-   
+        <ProfileForm />
+      
     </div>
   );
 }
