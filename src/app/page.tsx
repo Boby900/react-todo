fdfd3f283c4c51d78e39dd1c3bb91deea8c65a33
Boby900 <bobytiwari310@gmail.com>
2024-06-28 07:00:50 +0530

    refactor: Update import statement for ProfileForm component in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 
 import _React from 'react';
 // import { SignIn } from '@/components/sign-in';
-import  ProfileForm  from '@/components/Form';
+
+import ProfileForm from '../components/Form'
 export default function Home() {
   return (
     <div >
